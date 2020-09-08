05937ab6452af2ff91822f155da9dc133d10c391
syahjamal <sulaimansyahjamal@gmail.com>
2020-09-08 15:35:33 +0700

    mencegah slug kembar

--- routes/article.go
+++ routes/article.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gosimple/slug"
 	"github.com/syahjamal/gin-full-api/config"
@@ -38,16 +41,24 @@ func GetArticle(c *gin.Context) {
 }
 
 func PostArticle(c *gin.Context) {
+
+	var oldItem models.Article
+	slug := slug.Make(c.PostForm("title"))
+
+	if !config.DB.First(&oldItem, "slug = ?", slug).RecordNotFound() {
+		//generate string baru
+		slug = slug + "-" + strconv.FormatInt(time.Now().Unix(), 10)
+	}
+
 	item := models.Article{
 		Title:  c.PostForm("title"),
 		Desc:   c.PostForm("desc"),
 		Tag:    c.PostForm("tag"),
-		Slug:   slug.Make(c.PostForm("title")),
+		Slug:   slug,
 		UserID: uint(c.MustGet("jwt_user_id").(float64)),
 	}
 
 	//Mencegah slug sama, maka generate random slug
-
 	config.DB.Create(&item)
 
 	c.JSON(200, gin.H{
